625aeeed8faca398a545804b012885d39d2ebd89
wanjiruaisha <wanjiruaisha30@gmail.com>
2026-07-11 15:01:58 +0300

    refactor: reorganize imports and update routing structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
+import { Route, Routes } from "react-router-dom";
+
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 import Home from "@/pages/Home";
-// import Events from "@/pages/Events";
-import Contact from "@/pages/Contact";
+import Events from "@/pages/Events";
+import EventDetails from "@/pages/EventDetails";
+import BookEvent from "@/pages/BookEvent";
+import MyTickets from "@/pages/MyTickets";
 import Login from "@/pages/Login";
+import Signup from "@/pages/Signup";
+import Contact from "@/pages/Contact";
+import NotFound from "@/pages/NotFound";
 
 function App() {
   return (
-    <div className="min-h-screen bg-stone-50 text-stone-900">
+    <div className="flex min-h-screen flex-col bg-stone-50 text-stone-900 dark:bg-stone-950 dark:text-stone-100">
       <Navbar />
 
-      <main>
+      <main className="flex-1">
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/events" element={<Events />} /> */}
-          <Route path="/contact" element={<Contact />} />
+
+          <Route path="/events" element={<Events />} />
+
+          <Route
+            path="/events/:id"
+            element={<EventDetails />}
+          />
+
+          <Route
+            path="/book/:id"
+            element={<BookEvent />}
+          />
+
+          <Route
+            path="/my-tickets"
+            element={<MyTickets />}
+          />
+
           <Route path="/login" element={<Login />} />
+
+          <Route path="/signup" element={<Signup />} />
+
+          <Route path="/contact" element={<Contact />} />
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
 
